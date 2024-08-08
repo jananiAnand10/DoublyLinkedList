fb03fe4e653b6e63bc807eb021800ee229696dc6
jananiAnand10 <158706537+jananiAnand10@users.noreply.github.com>
2024-08-08 18:28:57 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,25 +1,63 @@
-public class Driver {
-public static void main(String[] args) {
-	LinkedList myLinkedList=new LinkedList(0);
-	myLinkedList.append(1);
-	myLinkedList.append(2);
-	myLinkedList.append(3);
-	myLinkedList.getHead();
-	myLinkedList.getTail();
-	myLinkedList.getLength();
-//	System.out.println(myLinkedList.removeLast().value);
-//	myLinkedList.prepend(6);
-//	myLinkedList.prepend(7);
-//	System.out.println(myLinkedList.removeFirst().value);
-//	myLinkedList.getHead();
-//	myLinkedList.getTail();
-//	myLinkedList.getLength();
-//	System.out.println(myLinkedList.get(1).value);
-//	System.out.println(myLinkedList.set(5, 0));
-//	System.out.println(myLinkedList.insert(2, 7));
-//	System.out.println(myLinkedList.remove(2).value);
-	myLinkedList.reverse();
-	myLinkedList.printList();
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
+        myDLL.append(3);
+        myDLL.append(4);
+        myDLL.append(5);
+
+        System.out.println("DLL before remove():");
+        myDLL.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myDLL.remove(2).value);
+        System.out.println("DLL after remove() in middle:");
+        myDLL.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myDLL.remove(0).value);
+        System.out.println("DLL after remove() of first node:");
+        myDLL.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myDLL.remove(2).value);
+        System.out.println("DLL after remove() of last node:");
+        myDLL.printList();
+
+
+	//additional code
+	System.out.println("DLL before swap:");
+        myDLL.printList();
+        
+        myDLL.swapFirstLast();
+        
+        System.out.println("\nDLL after swap:");
+        myDLL.printList();
+
+	//reverse
+	System.out.println("DLL before reverse:");
+        myDLL.printList();
+        
+        myDLL.reverse();
+        
+        System.out.println("\nDLL after reverse:");
+        myDLL.printList();
+
+	System.out.println("myDll before swapPairs:");
+        myDll.printList();
+
+        myDll.swapPairs();
+
+        System.out.println("\nmyDll after swapPairs:");
+        myDll.printList();
+
+	DoublyLinkedList myDLL1 = new DoublyLinkedList(1);
+        myDLL1.append(2);
+        myDLL1.append(3);
+        myDLL1.append(2);
+        myDLL1.append(1);
+        
+        System.out.println("myDLL1 isPalindrome:");
+        System.out.println( myDLL1.isPalindrome() );
 
 }
 }
